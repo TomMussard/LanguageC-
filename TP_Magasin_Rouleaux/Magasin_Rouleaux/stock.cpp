ace15e203079384febbe69e5693b68614b969b1b
TomMussard <90617490+TomMussard@users.noreply.github.com>
2022-10-14 10:59:33 +0200

    Add files via upload

--- TP_Magasin_Rouleaux/Magasin_Rouleaux/stock.cpp
+++ TP_Magasin_Rouleaux/Magasin_Rouleaux/stock.cpp
@@ -4,3 +4,40 @@ stock::stock()
 {
 
 }
+
+void stock::AjouterRouleau(const Rouleau &_nouveauRouleau)
+{
+    insert(_nouveauRouleau);
+}
+
+unsigned int stock::RechercherSerie(positionStock &it)
+{
+    unsigned int retour = 0;
+    if(size() < 6){
+        retour = size();
+        it = begin();
+    }
+    else {
+        retour = 6;
+        it = begin();
+    }
+    return retour;
+}
+
+
+void stock::Visualiser() const
+{
+    positionStock it;
+    int nb = {0};
+    for (it = begin(); it != end(); it++){
+        it->Visualiser();
+        nb++;
+    }
+    if (nb == 0)
+        cout << "Le magasin est vide" << endl;
+}
+
+void stock::SortirRouleau(positionStock &it)
+{
+    erase(it);
+}
--- TP_Magasin_Rouleaux/Magasin_Rouleaux/stock.h
+++ TP_Magasin_Rouleaux/Magasin_Rouleaux/stock.h
@@ -1,11 +1,21 @@
 #ifndef STOCK_H
 #define STOCK_H
+#include <iostream>
+#include <set>
+#include "rouleau.h"
+#include "rouleau.h"
 
 
-class stock
+typedef std::multiset<Rouleau>::iterator positionStock;
+
+class stock : public std::multiset<Rouleau>
 {
 public:
     stock();
+    void AjouterRouleau(const Rouleau &_nouveauRouleau);
+    unsigned int RechercherSerie(positionStock &it);
+    void SortirRouleau(positionStock &it);
+    void Visualiser() const;
 };
 
 #endif // STOCK_H
